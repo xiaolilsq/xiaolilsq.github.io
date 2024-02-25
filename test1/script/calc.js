13c07c08812542e884de6b30238ae6a76c899e7e
Siqi Li <2605059174@qq.com>
2024-02-25 21:41:27 +0800

    UPD

--- test1/script/calc.js
+++ test1/script/calc.js
@@ -2,6 +2,12 @@ var cnt = 0;
 function clickCalc(){
     ++cnt;
     document.getElementById("count").innerHTML=cnt;
+    if(cnt>=100){
+        document.getElementById("ImBut").onclick=stoImakf();
+    }
+}
+function stoImakf(){
+    document.getElementById("ImBut").innerHTML="stO Imakf Orz";
 }
 function clickCalc10(){
     cnt+=10;
